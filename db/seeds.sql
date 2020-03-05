420e9dcc5877290bad79b94e142fa30e69fb23bd
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-03-05 18:01:06 -0500

    DB setup done

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSER INTO burguers (burguer_name, devoured) VALUES ( Steak, 0);
-INSER INTO burguers (burguer_name, devoured) VALUES ( Chicken, 0);
-INSER INTO burguers (burguer_name, devoured) VALUES ( Double Steak, 0);
-INSER INTO burguers (burguer_name, devoured) VALUES ( BBQ, 0);
-INSER INTO burguers (burguer_name, devoured) VALUES ( Pulled Pork, 0);
+INSERT INTO burguers (burguer_name, devoured) VALUES ( 'Steak', 0);
+INSERT INTO burguers (burguer_name, devoured) VALUES ( 'Chicken', 0);
+INSERT INTO burguers (burguer_name, devoured) VALUES ( 'Double Steak', 0);
+INSERT INTO burguers (burguer_name, devoured) VALUES ( 'BBQ', 0);
+INSERT INTO burguers (burguer_name, devoured) VALUES ( 'Pulled Pork', 0);
