e3a28e1f3e1290ac6fde1ad51a288eb936aea246
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-03-06 14:09:49 -0500

    deleting comments

--- controllers/burguers_controller.js
+++ controllers/burguers_controller.js
@@ -22,7 +22,7 @@ router.post("/api/burguers", (req, res) => {
   ], [
     req.body.name, req.body.sleepy
   ], function(result) {
-    // Send back the ID of the new quote
+
     res.json({ id: result.insertId });
   });
 });
@@ -36,7 +36,7 @@ router.put("/api/burguers/:id", (req, res) => {
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+  
       return res.status(404).end();
     } else {
       res.status(200).end();
